4bd210ab97b70b4a40bc90f0f1e8b55cbf187ef0
hoshii <yoshito_hoshii@bita.jp>
2016-04-18 19:55:05 +0900

    スライドの実装

--- common/js/app.js
+++ common/js/app.js
@@ -9,4 +9,31 @@ $(function() {
 		$(this).addClass('current')
 	});
 
+	/* slider処理（slick） */
+	$('.responsible_slides').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+			},{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				}
+			}
+		]
+	});
+
+	$('.lazy-item').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+
 });
